149ba70843b75257790143d1f3d0161f1d248105
Konstantinos Panayiotou <klpanagi@gmail.com>
2018-12-08 17:57:35 +0200

    Fix #3

--- examples/subscriber.py
+++ examples/subscriber.py
@@ -22,7 +22,7 @@ def callback(msg, meta):
 if __name__ == '__main__':
     topic = sys.argv[1] if len(sys.argv) > 1 else 'robot_1.dummy'
     sub = amqp_common.SubscriberSync(
-        topic, callback=callback,
+        topic, on_message=callback,
         connection_params=amqp_common.ConnectionParameters(
             host='155.207.33.185', port='5672'),
         creds=amqp_common.Credentials('robot_1', 'r0b0t1'))
