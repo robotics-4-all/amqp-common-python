3742227c1ca341ed0ca8ed6bb1c94f751ee12646
Konstantinos Panayiotou <klpanagi@gmail.com>
2019-01-20 18:23:02 +0200

    .

--- examples/rpc_client.py
+++ examples/rpc_client.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
 from __future__ import print_function
@@ -7,23 +7,17 @@ import sys
 
 import amqp_common
 
-
 if __name__ == "__main__":
     rpc_name = sys.argv[1] if len(sys.argv) > 1 else 'rpc.mult'
 
     creds = amqp_common.Credentials(username='robot_1', password='r0b0t1')
-    conn_params = amqp_common.ConnectionParameters(host='155.207.33.185',
-                                                   port='5672')
+    conn_params = amqp_common.ConnectionParameters(
+        host='155.207.33.185', port='5672')
     rpc_client = amqp_common.RpcClient(
         rpc_name, creds=creds, connection_params=conn_params)
 
-    data = {
-        'a': 4,
-        'b': 13
-    }
+    data = {'a': 4, 'b': 13}
 
-    resp = rpc_client.call(data)
-    print(resp)
     rpc_client.debug = True
     resp = rpc_client.call(data)
     print(resp)
