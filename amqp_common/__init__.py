6b69688df05975d48e99336e103f9e312cac0fb3
Konstantinos Panayiotou <klpanagi@gmail.com>
2019-01-20 18:18:38 +0200

    Export SharedConnection and Rate

--- amqp_common/__init__.py
+++ amqp_common/__init__.py
@@ -2,10 +2,11 @@ from __future__ import absolute_import
 
 from .pubsub import PublisherSync, SubscriberSync
 from .rpc import RpcClient, RpcServer
-from .amqp_transport import Credentials, ConnectionParameters
+from .amqp_transport import Credentials, ConnectionParameters, SharedConnection
 from .timer import Timer
+from .rate import Rate
 
 __all__ = [
     'PublisherSync', 'SubscriberSync', 'RpcClient', 'RpcServer', 'Credentials',
-    'ConnectionParameters', 'Timer'
+    'ConnectionParameters', 'Timer', 'SharedConnection', 'Rate'
 ]
