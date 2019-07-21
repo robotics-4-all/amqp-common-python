a511fe7187baf020a02fdfe904853c40b8d88f22
Konstantinos Panayiotou <klpanagi@gmail.com>
2019-07-21 16:08:14 +0300

    Iteratively serialize composite messages

--- amqp_common/__init__.py
+++ amqp_common/__init__.py
@@ -5,8 +5,10 @@ from .rpc import RpcClient, RpcServer
 from .amqp_transport import Credentials, ConnectionParameters, SharedConnection
 from .timer import Timer
 from .rate import Rate
+from .msg import Message, HeaderMessage, FileMessage
 
 __all__ = [
     'PublisherSync', 'SubscriberSync', 'RpcClient', 'RpcServer', 'Credentials',
-    'ConnectionParameters', 'Timer', 'SharedConnection', 'Rate'
+    'ConnectionParameters', 'Timer', 'SharedConnection', 'Rate', 'Message',
+    'HeaderMessage', 'FileMessage'
 ]
--- amqp_common/msg.py
+++ amqp_common/msg.py
@@ -1,40 +1,76 @@
 # -*- coding: utf-8 -*-
 
 import time
+import json
+import base64
 
 
 class Message(object):
     """Abstract Message Class."""
 
-    __slots__ = []
-
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Constructor."""
-        pass
+        for key, value in kwargs.iteritems():
+            if hasattr(self.req, key):
+                setattr(self.req, key, value)
+            else:
+                raise AttributeError(''.join(self.__class__.__name__ +
+                    ' object does not have a property named ' + str(key)))
 
     def _to_dict(self):
         """Serialize message object to a dict."""
-        return {
-            k: getattr(self, k)
-            for k in self.__slots__ if not k.startswith('_')
-        }
+        _d = {}
+        for k in self.__slots__:
+            if not k.startswith('_'):
+                _prop = getattr(self, k)
+                if isinstance(_prop, Message):
+                    _d[k] = _prop._to_dict()
+                else:
+                    _d[k] = _prop
+        return _d
 
     def _from_dict(self, data_dict):
         """Fill message data fields from dict key-value pairs."""
         for key, val in data_dict.items():
             setattr(self, key, val)
 
+    def serialize_json(self):
+        return json.dumps(self._to_dict())
 
-class HeaderMsg(Message):
+    def serialize(self):
+        return self._to_dict()
+
+    def __eq__(self, other):
+        """! Equality method """
+        return self.serialize() == other.serialize()
+
+
+class HeaderMessage(Message):
     """Header message class."""
 
-    __slots__ = ['seq', 'timestamp' 'sender_id']
+    __slots__ = ['seq', 'timestamp', 'sender_id']
 
     counter = 0
 
-    def __init__(self, seq=None, timestamp=None):
+    def __init__(self, seq=None, timestamp=None, sender_id=''):
         """Constructor."""
-        HeaderMsg.counter = HeaderMsg.counter + 1 if seq is None else seq
-        self.seq = HeaderMsg.counter
+        HeaderMessage.counter = HeaderMessage.counter + 1 if seq is None else seq
+        self.seq = HeaderMessage.counter
         self.timestamp = int(time.time()) if timestamp is None else timestamp
-        self.sender_id = 'not-specified'
+        self.sender_id = sender_id
+
+
+class FileMessage(Message):
+
+    __slots__ = ['filename', 'header', 'data']
+
+    def __init__(self, filename='', data=[]):
+        self.filename = filename
+        self.data = data
+        self.header = HeaderMessage()
+
+    def load_from_file(self, filepath):
+        with open(filepath, 'rb') as f:
+            fdata = f.read()
+            b64 = base64.b64encode(fdata)
+            self.data = b64
